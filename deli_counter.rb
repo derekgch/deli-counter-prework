1afa23ccbef40e95712212f5aed12f2000c1ecd5
derek chen <derek.g.chen@gmail.com>
2018-03-21 18:53:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+
+#line there is nobody in line should say the line is empty
+#line there are people in line should display the current line
+def line(arrary)
+end
+
+#take_a_number there is nobody in line should add a person to the line
+#take_a_number there are already people in line should add a person to the end of the line
+#take_a_number adding multiple people in a row should correctly build the line
+
+def take_a_number()
+end
+
+#now_serving there are no people in line should say that the line is empty
+#now_serving there are people in line should serve the first person in line and remove them from the queue
+
+def now_serving()
+end
